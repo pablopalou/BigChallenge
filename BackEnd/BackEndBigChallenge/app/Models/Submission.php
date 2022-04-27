2e463945677dd994a1c223206b23622b558ea6c5
Pablo Palou <pablo.palou@lightit.io>
2022-04-27 09:57:31 -0300

    create models

--- BackEnd/BackEndBigChallenge/app/Models/Submission.php
+++ BackEnd/BackEndBigChallenge/app/Models/Submission.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Submission extends Model
 {
     use HasFactory;
+
+    // I think I don't need to specify the third parameter as it is called id
+    // Should I relate this submission to PatientInformation or User?
+
+    // if i choose to put user, i must put user_id as the second parameter and in the third, i
+    // have to specify the column of the Submission i am relating to.
+    public function patient(){
+        return $this->belongsTo(User::class, 'user_id', 'patient_id');
+    }
+
+    public function doctor(){
+        return $this->belongsTo(User::class, 'user_id', 'doctor_id');
+    }
 }
