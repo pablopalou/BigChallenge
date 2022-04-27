81478ef76d429de662414b76ee58d0c03e3735b3
Pablo Palou <pablo.palou@lightit.io>
2022-04-27 08:10:09 -0300

    migrations done

---
+++ BackEnd/BackEndBigChallenge/database/migrations/2022_04_27_100653_create_doctor_information_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor_information', function (Blueprint $table) {
+            // $table->id();
+            $table->timestamps();
+            $table->integer("grade")->default(1);
+            $table->string("speciality")->default("General");
+            $table->foreignId("user_id")->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor_information');
+    }
+};
