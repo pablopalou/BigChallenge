86ce10b0eacbed16b8c22cad658a0ded82c8b415
pablopalou <pablopalou@users.noreply.github.com>
2022-04-28 15:08:35 +0000

    Fix styling

--- BackEnd/BackEndBigChallenge/database/factories/SubmissionFactory.php
+++ BackEnd/BackEndBigChallenge/database/factories/SubmissionFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\DoctorInformation;
 use App\Models\PatientInformation;
 use App\Models\Submission;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
