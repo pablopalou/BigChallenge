5e67a7a9a210510de32f3aac644fbf0db2a7add9
Pablo Palou <pablo.palou@lightit.io>
2022-04-29 10:26:41 -0300

    fix Response of RegisterController

--- BackEnd/BackEndBigChallenge/app/Http/Controllers/RegisterController.php
+++ BackEnd/BackEndBigChallenge/app/Http/Controllers/RegisterController.php
@@ -12,8 +12,8 @@ class RegisterController extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\RegisterRequest  $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function __invoke(RegisterRequest $request): JsonResponse
     {
