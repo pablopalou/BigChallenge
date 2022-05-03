d8ad7438454751a3f1a84180157d306c8669fb92
pablopalou <64150071+pablopalou@users.noreply.github.com>
2022-05-03 15:06:08 -0300

    Feature/2nw7c3e/login tests (#14)
    * Add more tests to Login
    
    * Fix styling
    
    * Delete .php-cs-fixer.cache
    
    * Fix styling
    
    Co-authored-by: Pablo Palou <pablo.palou@lightit.io>
    Co-authored-by: pablopalou <pablopalou@users.noreply.github.com>

--- BackEnd/BackEndBigChallenge/tests/Feature/LoginTest.php
+++ BackEnd/BackEndBigChallenge/tests/Feature/LoginTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
@@ -24,5 +25,67 @@ class LoginTest extends TestCase
         $response->assertJson([
             'status' => 200,
             'message' => 'User logged succesfully', ]);
+    }
+
+    /**
+     * @dataProvider wrongCredentialsProvider
+     */
+    public function test_log_in_wrong_credentials($user)
+    {
+        User::factory()->create(['email'=>'pablo@pablo.com', 'password'=> 'passwordPablo']);
+        $response = $this->postJson('/api/login', $user);
+        $response->assertJson(['message' => 'The provided credentials are incorrect.']);
+    }
+
+    public function wrongCredentialsProvider(): array
+    {
+        return [
+            ['mustFailPassword' => ['email'=>'pablo@pablo.com', 'password'=> 'wrongPassword']],
+            ['mustFailEmail' => ['email'=>'notanemail@pablo.com', 'password'=> 'passwordPablo']],
+            ['mustFailBoth' => ['email'=>'notanemail@pablo.com', 'password'=> 'wrongPassword']],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidCredentialsProvider
+     */
+    public function test_log_in_invalid_credentials($user)
+    {
+        User::factory()->create(['email'=>'pablo@pablo.com', 'password'=> 'passwordPablo']);
+        $response = $this->postJson('/api/login', $user);
+        // DOUBT
+        // I found on (https://laracasts.com/discuss/channels/laravel/l52-change-laravel-http-status-code-for-validation) that the failed validation
+        // status was 422. Is this test OK or do i have to do sth more?
+        $response->assertStatus(422);
+    }
+
+    public function invalidCredentialsProvider(): array
+    {
+        return [
+            ['mustFailPassword' => ['email'=>'valid@fd.fds', 'password'=> 'short']],
+            ['mustFailEmail' => ['email'=>'notanemail@pablo.com', 'password'=> 'passwordPablo']],
+            ['mustFailBoth' => ['email'=>'notAValidEmail@.', 'password'=> 'a']],
+        ];
+    }
+
+    /**
+     * @dataProvider usersProvider
+     */
+    public function test_log_in_if_already_logged_in($user)
+    {
+        Sanctum::actingAs(User::factory()->create());
+        $response = $this->postJson('/api/login', $user);
+        // SAME DOUBT HERE (Do I need to do anything more in this test?)
+        $response->assertStatus(302);
+    }
+
+    public function usersProvider(): array
+    {
+        return [
+            ['mustFailPassword' => ['email'=>'valid@fd.fds', 'password'=> 'short']],
+            ['mustFailEmail' => ['email'=>'notanemail@pablo.com', 'password'=> 'passwordPablo']],
+            ['mustFailBoth' => ['email'=>'notAValidEmail@.', 'password'=> 'a']],
+            ['mustFailBoth' => ['email'=>'', 'password'=> '']],
+        ];
     }
 }
