8a7119e135b72da4468988f624dc6924fa06c288
pablopalou <pablopalou@users.noreply.github.com>
2022-05-03 12:41:49 +0000

    Fix styling

--- BackEnd/BackEndBigChallenge/app/Models/Submission.php
+++ BackEnd/BackEndBigChallenge/app/Models/Submission.php
@@ -9,21 +9,22 @@ class Submission extends Model
 {
     use HasFactory;
 
+    const STATUS_PENDING = 'Pending';
+    const STATUS_IN_PROGRESS = 'In progress';
+    const STATUS_READY = 'Ready';
 
-    const STATUS_PENDING = "Pending";
-    const STATUS_IN_PROGRESS = "In progress";
-    const STATUS_READY = "Ready";
-    
     // I think I don't need to specify the third parameter as it is called id
     // Should I relate this submission to PatientInformation or User?
 
     // if i choose to put user, i must put user_id as the second parameter and in the third, i
     // have to specify the column of the Submission i am relating to.
-    public function patient(){
+    public function patient()
+    {
         return $this->belongsTo(User::class, 'user_id', 'patient_id');
     }
 
-    public function doctor(){
+    public function doctor()
+    {
         return $this->belongsTo(User::class, 'user_id', 'doctor_id');
     }
 }
