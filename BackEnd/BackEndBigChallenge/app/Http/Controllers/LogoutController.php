26be7477c9193797e03f96ce7ded9b91d7df77e0
Pablo Palou <pablo.palou@lightit.io>
2022-05-03 14:51:40 -0300

    Add Logout Tests

--- BackEnd/BackEndBigChallenge/app/Http/Controllers/LogoutController.php
+++ BackEnd/BackEndBigChallenge/app/Http/Controllers/LogoutController.php
@@ -9,14 +9,11 @@ class LogoutController extends Controller
 {
     public function __invoke():JsonResponse
     {
-        // DOUBT
-        // It is necessary to make logout after deleting the token? Why | Why not?
         // QUESTION
         // Why   Auth::user()->tokens()->delete(); is no working anymore? It says: 'tokens() is not a function'
         Auth::user()->tokens->each(function ($token, $key) {
             $token->delete();
         });
-        Auth::logout();
 
         return response()->json([
             'status' => 200,
