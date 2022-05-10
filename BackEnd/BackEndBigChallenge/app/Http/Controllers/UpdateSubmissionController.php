e9656d4e882dd2ac2f48bd97e201949542ab2856
Pablo Palou <ppalou2000@icloud.com>
2022-05-10 07:47:58 -0300

    change Submission to Symptoms

--- BackEnd/BackEndBigChallenge/app/Http/Controllers/UpdateSubmissionController.php
+++ BackEnd/BackEndBigChallenge/app/Http/Controllers/UpdateSubmissionController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UpdateSymptomsRequest;
 use App\Http\Resources\SubmissionResource;
-use App\Http\Resources\SymptomsResource;
 use App\Models\Submission;
-use App\Models\Symptoms;
 
-class UpdateSymptomsController extends Controller
+class UpdateSubmissionController extends Controller
 {
     public function __invoke(UpdateSymptomsRequest $request, Submission $submission):SubmissionResource
     {
