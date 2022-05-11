c6ca185ef413ed508c0767dd6239686a079da00e
pablopalou <pablopalou@users.noreply.github.com>
2022-05-11 14:48:18 +0000

    Fix styling

--- BackEnd/BackEndBigChallenge/app/Http/Requests/CreateSubmissionRequest.php
+++ BackEnd/BackEndBigChallenge/app/Http/Requests/CreateSubmissionRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use App\Models\Submission;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class CreateSubmissionRequest extends FormRequest
 {
