c45a7ea0a89de693e54e2f46c26014416ad12e45
Pablo Palou <ppalou2000@icloud.com>
2022-05-12 09:35:44 -0300

    fix styling

--- BackEnd/BackEndBigChallenge/app/Http/Controllers/DeleteSubmissionController.php
+++ BackEnd/BackEndBigChallenge/app/Http/Controllers/DeleteSubmissionController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Requests\DeleteSubmissionRequest;
 use App\Models\Submission;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class DeleteSubmissionController extends Controller
 {
     public function __invoke(DeleteSubmissionRequest $request, Submission $submission): JsonResponse
     {
         $submission->delete();
+
         return response()->json([
             'status' => 200,
             'message' => 'Submission deleted successfully',
