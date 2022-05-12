10936790c2b987f71d6c67ea0eb357646b9a1932
pablopalou <pablopalou@users.noreply.github.com>
2022-05-12 00:08:37 +0000

    Fix styling

--- BackEnd/BackEndBigChallenge/tests/Feature/GetSubmissionTest.php
+++ BackEnd/BackEndBigChallenge/tests/Feature/GetSubmissionTest.php
@@ -8,16 +8,16 @@ use App\Models\Submission;
 use App\Models\User;
 use Database\Seeders\RolesSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class GetSubmissionTest extends TestCase
 {
     use RefreshDatabase;
+
     public function test_get_submission_being_doctor()
     {
-        (new RolesSeeder())->run();       
+        (new RolesSeeder())->run();
         $submission = Submission::factory()->create(['state' => Submission::STATUS_IN_PROGRESS]);
         $userDoctor = $submission->doctor;
         $userDoctor->assignRole('doctor');
@@ -28,7 +28,7 @@ class GetSubmissionTest extends TestCase
         $response->assertJson(['message' => 'Received Submission Successfully',
                                 'data' =>  ['symptoms' => $submission->symptoms,
                                             'state' => $submission->state,
-                                            'prescriptions' => $submission->prescriptions, 
+                                            'prescriptions' => $submission->prescriptions,
                                             'doctor' => ['id' => $submission->doctor_id],
                                             'patient' => ['id' => $submission->patient_id],
                                             ],
@@ -37,7 +37,7 @@ class GetSubmissionTest extends TestCase
 
     public function test_get_submission_being_patient()
     {
-        (new RolesSeeder())->run();       
+        (new RolesSeeder())->run();
         $submission = Submission::factory()->create(['state' => Submission::STATUS_IN_PROGRESS]);
         $userPatient = $submission->patient;
         $userPatient->assignRole('patient');
@@ -47,7 +47,7 @@ class GetSubmissionTest extends TestCase
         $response->assertJson(['message' => 'Received Submission Successfully',
                                 'data' =>  ['symptoms' => $submission->symptoms,
                                             'state' => $submission->state,
-                                            'prescriptions' => $submission->prescriptions, 
+                                            'prescriptions' => $submission->prescriptions,
                                             'doctor' => ['id' => $submission->doctor_id],
                                             'patient' => ['id' => $submission->patient_id],
                                             ],
@@ -56,7 +56,7 @@ class GetSubmissionTest extends TestCase
 
     public function test_get_submission_not_authorized_patient()
     {
-        (new RolesSeeder())->run();       
+        (new RolesSeeder())->run();
         Submission::factory()->create(['state' => Submission::STATUS_IN_PROGRESS]);
         $userPatient = User::factory()->has(PatientInformation::factory())->create();
         $userPatient->assignRole('patient');
@@ -67,7 +67,7 @@ class GetSubmissionTest extends TestCase
 
     public function test_get_submission_not_authorized_doctor()
     {
-        (new RolesSeeder())->run();       
+        (new RolesSeeder())->run();
         Submission::factory()->create(['state' => Submission::STATUS_IN_PROGRESS]);
         $userDoctor = User::factory()->has(DoctorInformation::factory())->create();
         $userDoctor->assignRole('doctor');
