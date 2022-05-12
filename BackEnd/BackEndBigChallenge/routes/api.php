51a617c339eca39b06ea4035f9d51c92be6dc176
pablopalou <64150071+pablopalou@users.noreply.github.com>
2022-05-12 11:06:22 -0300

    Feature/2r0wnfh/take submission (#29)
    * Add Take submission Controller,Request and Tests
    
    * Fixing styles
    
    * Feature/2r0wprq/delete submission (#30)
    
    * Add Delete submission Controller,Request and Tests
    
    * Fix styling
    
    * fix styling
    
    * Fix Larastan
    
    * Delete comment
    
    Co-authored-by: pablopalou <pablopalou@users.noreply.github.com>
    
    Co-authored-by: pablopalou <pablopalou@users.noreply.github.com>

--- BackEnd/BackEndBigChallenge/routes/api.php
+++ BackEnd/BackEndBigChallenge/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CreateSubmissionController;
+use App\Http\Controllers\DeleteSubmissionController;
 use App\Http\Controllers\GetDoctorInformationController;
 use App\Http\Controllers\GetPatientInformationController;
 use App\Http\Controllers\GetSubmissionController;
@@ -8,6 +9,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ResendVerificationEmailController;
+use App\Http\Controllers\TakeSubmissionController;
 use App\Http\Controllers\UpdateDoctorInformationController;
 use App\Http\Controllers\UpdatePatientInformationController;
 use App\Http\Controllers\UpdateSymptomsController;
@@ -57,3 +59,7 @@ Route::get('/getDoctorInformation/{doctorInformation}', GetDoctorInformationCont
 Route::get('/getPatientInformation/{patientInformation}', GetPatientInformationController::class)->middleware('auth:sanctum');
 
 Route::get('/submission/{submission}', GetSubmissionController::class)->middleware('auth:sanctum');
+
+Route::post('/submission/{submission}/take', TakeSubmissionController::class)->middleware('auth:sanctum', 'role:doctor');
+
+Route::delete('/submission/{submission}', DeleteSubmissionController::class)->middleware('auth:sanctum', 'role:patient');
---
+++ BackEnd/BackEndBigChallenge/tests/Feature/TakeSubmissionTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\DoctorInformation;
+use App\Models\PatientInformation;
+use App\Models\Submission;
+use App\Models\User;
+use Database\Seeders\RolesSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class TakeSubmissionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_doctor_can_take_pending_submission()
+    {
+        (new RolesSeeder)->run();
+        $doctor = User::factory()->has(DoctorInformation::factory())->create();
+        $doctor->assignRole('doctor');
+        Sanctum::actingAs($doctor);
+        Submission::factory()->create(['state' => Submission::STATUS_PENDING]);
+        $response = $this->postJson('/api/submission/1/take');
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 200,
+            'message' => 'Doctor took the submission successfully',
+        ]);
+    }
+
+    public function test_doctor_can_not_take_in_progress_submission()
+    {
+        (new RolesSeeder)->run();
+        $doctor = User::factory()->has(DoctorInformation::factory())->create();
+        $doctor->assignRole('doctor');
+        Sanctum::actingAs($doctor);
+        Submission::factory()->create(['state' => Submission::STATUS_IN_PROGRESS]);
+        $response = $this->postJson('/api/submission/1/take');
+        $response->assertStatus(403);
+    }
+
+    public function test_doctor_can_not_take_ready_submission()
+    {
+        (new RolesSeeder)->run();
+        $doctor = User::factory()->has(DoctorInformation::factory())->create();
+        $doctor->assignRole('doctor');
+        Sanctum::actingAs($doctor);
+        Submission::factory()->create(['state' => Submission::STATUS_READY]);
+        $response = $this->postJson('/api/submission/1/take');
+        $response->assertStatus(403);
+    }
+
+    public function test_patient_can_not_take_pending_submission()
+    {
+        (new RolesSeeder)->run();
+        $patient = User::factory()->has(PatientInformation::factory())->create();
+        $patient->assignRole('patient');
+        Sanctum::actingAs($patient);
+        Submission::factory()->create(['state' => Submission::STATUS_PENDING]);
+        $response = $this->postJson('/api/submission/1/take');
+        $response->assertStatus(403);
+    }
+
+    public function test_guest_can_not_take_pending_submission()
+    {
+        Submission::factory()->create(['state' => Submission::STATUS_PENDING]);
+        $response = $this->postJson('/api/submission/1/take');
+        $response->assertStatus(401);
+    }
+}
