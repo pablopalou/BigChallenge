8d9172ab30cb9cb31b600e98cdef466b35549c47
Pablo Palou <ppalou2000@icloud.com>
2022-05-12 09:41:20 -0300

    Fix Larastan

--- BackEnd/BackEndBigChallenge/app/Http/Requests/DeleteSubmissionRequest.php
+++ BackEnd/BackEndBigChallenge/app/Http/Requests/DeleteSubmissionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Submission;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,6 +10,7 @@ class DeleteSubmissionRequest extends FormRequest
 {
     public function authorize(): bool
     {
+        /** @var Submission $submission */
         $submission = $this->route('submission');
 
         return $submission->patient_id == Auth::user()->id;
--- BackEnd/BackEndBigChallenge/app/Http/Requests/TakeSubmissionRequest.php
+++ BackEnd/BackEndBigChallenge/app/Http/Requests/TakeSubmissionRequest.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Submission;
 use Illuminate\Foundation\Http\FormRequest;
 
 class TakeSubmissionRequest extends FormRequest
 {
     public function authorize(): bool
     {
+        /** @var Submission $submission */
         $submission = $this->route('submission');
 
         return !isset($submission->doctor_id);
