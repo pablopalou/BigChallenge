774745d31e9fc6e02242af5d1943f8a1c5d61629
Pablo Palou <ppalou2000@icloud.com>
2022-05-17 20:56:56 -0300

    Add test for UploadPrescription

--- BackEnd/BackEndBigChallenge/app/Http/Controllers/UploadPrescriptionController.php
+++ BackEnd/BackEndBigChallenge/app/Http/Controllers/UploadPrescriptionController.php
@@ -14,21 +14,23 @@ class UploadPrescriptionController extends Controller
     public function __invoke(UploadPrescriptionRequest $request, Submission $submission): JsonResponse
     {
         $file = $request->file('prescriptions');
-        $fileName = (string) SupportStr::uuid();
+        // The fileName will be uuid, that is a unique identifier of the files.
+        $uuid = (string) SupportStr::uuid();
         $folder = config('filesystems.disks.do.folder');
         Storage::put(
-            "{$folder}/{$fileName}",
+            "{$folder}/{$uuid}",
             file_get_contents($file)
         );
         
         // Now I have to update the submission
-        $submission->prescriptions = $fileName;
+        $submission->prescriptions = $uuid;
         $submission->save();
 
         // @TODO: Make event to Notificate patient that a prescription has been made and dispatch it here. 
         
         return response()->json([
             'message' => 'File uploaded successfully',
+            'uuid' => $uuid,
         ]);
     }
 }
--- BackEnd/BackEndBigChallenge/tests/Feature/UploadPrescriptionTest.php
+++ BackEnd/BackEndBigChallenge/tests/Feature/UploadPrescriptionTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class UploadPrescriptionTest extends TestCase
@@ -15,12 +16,54 @@ class UploadPrescriptionTest extends TestCase
     public function test_prescription_save_successfully()
     {
         $submission = Submission::factory()->inProgress()->create();
+        Sanctum::actingAs($submission->doctor);
         Storage::fake('do');
         $response = $this->postJson("/api/submission/{$submission->id}/prescription", [
             'prescriptions' => UploadedFile::fake()->create('test.txt'),
         ]);
-        // $this->assertFileExists(Storage::disk('do')->"pablopalou/text.txt");
-        $this->assertTrue(Storage::disk('do')->exists("pablopalou/text.txt"));
-        // Storage::disk('do')->assertExists("pablopalou/text.txt");
+        $response->assertJson(['message' => 'File uploaded successfully']);
+        $this->assertTrue(Storage::disk('do')->exists("pablopalou/{$response->json()['uuid']}"));
+    }
+
+    public function test_guest_tryng_to_upload_prescription()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        Storage::fake('do');
+        $response = $this->postJson("/api/submission/{$submission->id}/prescription", [
+            'prescriptions' => UploadedFile::fake()->create('test.txt'),
+        ]);
+        $response->assertStatus(401);
+    }
+
+    public function test_patient_trying_to_upload_prescription()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        Sanctum::actingAs($submission->patient);
+        Storage::fake('do');
+        $response = $this->postJson("/api/submission/{$submission->id}/prescription", [
+            'prescriptions' => UploadedFile::fake()->create('test.txt'),
+        ]);
+        $response->assertStatus(403);
+    }
+
+    public function test_prescription_must_be_a_file()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        Sanctum::actingAs($submission->doctor);
+        Storage::fake('do');
+        $response = $this->postJson("/api/submission/{$submission->id}/prescription", [
+            'prescriptions' => 'Not a file',
+        ]);
+        $response->assertStatus(422);
+    }
+    public function test_prescription_must_be_txt()
+    {
+        $submission = Submission::factory()->inProgress()->create();
+        Sanctum::actingAs($submission->doctor);
+        Storage::fake('do');
+        $response = $this->postJson("/api/submission/{$submission->id}/prescription", [
+            'prescriptions' => UploadedFile::fake()->create('test.png'),
+        ]);
+        $response->assertStatus(422);
     }
 }
