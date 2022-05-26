b96c7ac4c082a7e80cf679861ee24c6f8699829f
pablopalou <pablopalou@users.noreply.github.com>
2022-05-26 17:55:05 +0000

    Fix styling

--- BackEnd/BackEndBigChallenge/tests/Feature/GetPrescriptionTest.php
+++ BackEnd/BackEndBigChallenge/tests/Feature/GetPrescriptionTest.php
@@ -5,18 +5,18 @@ namespace Tests\Feature;
 use App\Models\Submission;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class GetPrescriptionTest extends TestCase
 {
     use RefreshDatabase;
+
     public function test_patient_can_get_prescription_file()
     {
         $this->markTestSkipped('This test is skipped');
         $submission = Submission::factory()->ready()->create();
         $this->actingAs($submission->patient);
-        $response = $this->getJson('api/submission/prescription/'.$submission->id);
+        $response = $this->getJson('api/submission/prescription/' . $submission->id);
         $response->assertStatus(200);
         $response->assertJsonStructure([
             'status',
@@ -29,7 +29,7 @@ class GetPrescriptionTest extends TestCase
         $this->markTestSkipped('This test is skipped');
         $submission = Submission::factory()->ready()->create();
         $this->actingAs($submission->doctor);
-        $response = $this->getJson('api/submission/prescription/'.$submission->id);
+        $response = $this->getJson('api/submission/prescription/' . $submission->id);
         $response->assertStatus(200);
         $response->assertJsonStructure([
             'status',
@@ -44,14 +44,14 @@ class GetPrescriptionTest extends TestCase
         $this->actingAs($patient);
         $response = $this->getJson('api/submission/prescription/' . $submission->id);
         $response->assertForbidden();
-    } 
+    }
 
     public function test_doctor_cannot_get_prescription_file_if_not_ready()
     {
         $submission = Submission::factory()->inProgress()->create();
         $doctor = $submission->doctor;
         $this->actingAs($doctor);
-        $response = $this->getJson('api/submission/prescription/'.$submission->id);
+        $response = $this->getJson('api/submission/prescription/' . $submission->id);
         $response->assertForbidden();
     }
 
@@ -60,7 +60,7 @@ class GetPrescriptionTest extends TestCase
         $submission = Submission::factory()->ready()->create();
         $patient = User::factory()->patient()->create();
         $this->actingAs($patient);
-        $response = $this->getJson('api/submission/prescription/'.$submission->id);
+        $response = $this->getJson('api/submission/prescription/' . $submission->id);
         $response->assertForbidden();
     }
 
@@ -69,14 +69,14 @@ class GetPrescriptionTest extends TestCase
         $submission = Submission::factory()->ready()->create();
         $doctor = User::factory()->doctor()->create();
         $this->actingAs($doctor);
-        $response = $this->getJson('api/submission/prescription/'.$submission->id);
+        $response = $this->getJson('api/submission/prescription/' . $submission->id);
         $response->assertForbidden();
     }
 
     public function test_guest_cannot_get_prescription_file()
     {
         $submission = Submission::factory()->ready()->create();
-        $response = $this->getJson('api/submission/prescription/'.$submission->id);
+        $response = $this->getJson('api/submission/prescription/' . $submission->id);
         $response->assertUnauthorized();
     }
 }
