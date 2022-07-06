b9d41efb510a30ec277270f83d7eaa849f2427b1
Pablo Palou <ppalou2000@icloud.com>
2022-07-06 13:58:20 -0300

    Modify Markdown mailable

--- BackEnd/BackEndBigChallenge/resources/views/emails/prescription/mail.blade.php
+++ BackEnd/BackEndBigChallenge/resources/views/emails/prescription/mail.blade.php
@@ -1,12 +1,11 @@
 @component('mail::message')
-# Introduction
+Hi!
 
-The body of your message.
+One of your submissions is Ready now! Check your prescription in the page.
 
-@component('mail::button', ['url' => ''])
-Button Text
+@component('mail::button', ['url' => 'localhost:3000/login'])
+Login 
 @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+Thanks<br>
 @endcomponent
